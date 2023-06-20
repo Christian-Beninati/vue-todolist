284b0a48a172a692dc750cd1b43950c5175d16b6
Christian-Beninati <christianbeninati.info@gmail.com>
2023-06-20 15:22:00 +0100

    MILESTONE 1

--- js/script.js
+++ js/script.js
@@ -8,11 +8,19 @@ const app = createApp({
     // dati iniziali
     data(){               
         return{
-            
+            tasks: [
+            { id: 1, done: false, text: 'Fare la Milestone 1'},
+            { id: 2, done: false, text: 'Fare la Milestone 2'},
+            { id: 3, done: false, text: 'Fare la Milestone 3'},
+            { id: 4, done: false, text: 'Fare i Bonus'},
+            ],
         }
     },
-
-
+    methods: {
+        toggleTaskStatus(task) {
+            task.done = !task.done;
+        },
+    },
 });
 
 // Monto nell'elemento (di partenza)

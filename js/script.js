67e6079c05825e103d96febc203800ced8517cbe
Christian-Beninati <christianbeninati.info@gmail.com>
2023-06-20 20:48:49 +0100

    filter-search-task

--- js/script.js
+++ js/script.js
@@ -10,16 +10,33 @@ const app = createApp({
         return{
             // proprietà per memorizzare il testo del nuovo task
             newTaskText: '',
+
+            // Testo di ricerca
+            searchText: '',
+
             // Array di oggetti
             tasks: [
             { id: 1, done: true, text: 'Fare la Milestone 1'},
             { id: 2, done: true, text: 'Fare la Milestone 2'},
             { id: 3, done: true, text: 'Fare la Milestone 3'},
             { id: 4, done: true, text: 'Fare i Bonus'},
-            { id: 5, done: false, text: 'Fare il SuperBonus'},
+            { id: 5, done: true, text: 'Fare il SuperBonus'},
+            { id: 6, done: false, text: 'Rivedere la lezione di oggi'},
             ],
         }
     },
+
+    computed: {
+        // filtro per cercare all'interno dei task
+        filteredTasks() {
+            const search = this.searchText.toLowerCase();
+            
+          return this.tasks.filter((task) => {
+            return task.text.toLowerCase().includes(search);
+          });
+        },
+      },
+
     methods: {
         // Cambio stato (done false - done true)
         toggleTaskStatus(task) {
